83947458856a5655ac377bdcbfd44177d734e1c2
pandawaa <pallakangoding@gmail.com>
2024-02-08 15:37:10 +0800

    feat: Discover

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,28 +1,38 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Error, Loader, SongCard } from "../components"
 import { genres } from "../assets/constants"
-import { useGetTopChartsQuery } from "../redux/services/shazamCore"
+import {
+  useGetSongsByGenreQuery,
+  useGetTopChartsQuery,
+} from "../redux/services/shazamCore"
+import { selectGenreListId } from "../redux/features/playerSlice"
 
 const Discover = () => {
   const dispatch = useDispatch()
+  const { genreListId } = useSelector((state) => state.player)
   const { activeSong, isPlaying } = useSelector((state) => state.player)
-  const { data, isFetching, error } = useGetTopChartsQuery()
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  )
 
   if (isFetching) return <Loader title="Loading songs..." />
 
   if (error) return <Error />
 
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white">Discover</h2>
+        <h2 className="font-bold text-3xl text-white">Discover {genreTitle}</h2>
+
         <select
-          onChange={() => {}}
-          value=""
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
+          className="bg-black text-white p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
-            <option key={genre.key} value={genre.value}>
+            <option key={genre.value} value={genre.value}>
               {genre.title}
             </option>
           ))}
