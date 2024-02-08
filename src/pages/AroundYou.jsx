313e251fa038c7c4e88c7d4200b6bb4967ae731d
pandawaa <pallakangoding@gmail.com>
2024-02-08 15:37:45 +0800

    feat: Arround You

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore"
+import axios from "axios"
+import { Error, Loader, SongCard } from "../components"
 
-const CountryTracks = () => <div>CountryTracks</div>;
+const CountryTracks = () => {
+  const [country, setCountry] = useState("")
+  const [loading, setLoading] = useState(true)
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country)
 
-export default CountryTracks;
+  useEffect(() => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${
+          import.meta.env.VITE_GEO_API_KEY
+        }`
+      )
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false))
+  }, [country])
+
+  if (isFetching && loading)
+    return <Loader title="Loading Songs around you..." />
+
+  if (error && country !== "") return <Error />
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around you <span className="font-black">{country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CountryTracks
