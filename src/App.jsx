87ce92c8bfb6de961036201b21f1b5fb41478185
pandawaa <pallakangoding@gmail.com>
2024-02-08 13:14:19 +0800

    modified App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { useSelector } from "react-redux"
+import { Route, Routes } from "react-router-dom"
 
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from "./components"
+import {
+  ArtistDetails,
+  TopArtists,
+  AroundYou,
+  Discover,
+  Search,
+  SongDetails,
+  TopCharts,
+} from "./pages"
 
 const App = () => {
-  const { activeSong } = useSelector((state) => state.player);
+  const { activeSong } = useSelector((state) => state.player)
 
   return (
     <div className="relative flex">
@@ -37,7 +45,7 @@ const App = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
